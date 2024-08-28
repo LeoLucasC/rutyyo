4e24fcbcdd705084f0ed4c2a7e20ae6873b968c3
LeoLucasC <125475720+LeoLucasC@users.noreply.github.com>
2024-08-27 23:20:23 -0500

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,2 +1,20 @@
+n = int(input("COLOCA EL NUMERO: "))
 
-print("xd")
+def es_primo(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+resultado = es_primo(n)
+
+
+if resultado:
+    print(f"{n} es un número primo.")
+else:
+    print(f"{n} no es un número primo.")
+
+
